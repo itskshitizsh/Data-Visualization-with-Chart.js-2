f849d39c9a9c1c68464869a2c993a7892dc0449f
Kshitiz Sharma <skshitiz@vmware.com>
2022-07-31 12:39:36 +0530

    explore polarArea and radar

--- site.js
+++ site.js
@@ -2,29 +2,33 @@
 (
     async () => {
         var apiData = await getApiData();
-        // const branches = apiData.map(o => o.branch); // Array of the values based on the property specified. 
-        // const revenues2020 = apiData.map(o => o.revenue2020);
-        // const revenues2019 = apiData.map(o => o.revenue2019);
-        // const revenues2018 = apiData.map(o => o.revenue2018);
+        const branches = apiData.map(o => o.branch); // Array of the values based on the property specified. 
+        const revenues2020 = apiData.map(o => o.revenue2020);
+        const revenues2019 = apiData.map(o => o.revenue2019);
+        const revenues2018 = apiData.map(o => o.revenue2018);
         const colorPool = ["purple", "red", "green", "blue", "orange"]
-
-        let datasets = [];
-        apiData.forEach((item, index) => {
-            datasets.push({
-                label: item.branch,
-                data: [item.revenue2018, item.revenue2019, item.revenue2020],
-                borderColor: colorPool[index],
-                fill: false
-            });
-            
-        });
-
         const ctx = document.getElementById('chart');
         new Chart(ctx, {
-            type: 'line',
+            // type: 'polarArea', // SCALE SHOWS THE VALUE
+            type: 'radar', // Each segment is the whole dataset
             data: {
-                labels: [2018, 2019, 2020],
-                datasets: datasets
+                labels: branches,
+                datasets: [
+                    {
+                        backgroundColor: "red",
+                        label: "Revenues - 2018",
+                        data: revenues2018
+                    },                    {
+                        backgroundColor: "purple",
+                        label: "Revenues - 2019",
+                        data: revenues2019
+                    },
+                    {
+                        backgroundColor: "green",
+                        label: "Revenues - 2020",
+                        data: revenues2020
+                    }
+                ]
             }
         });
 
