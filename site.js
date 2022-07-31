8f7461b421237ebf2d681f82687d40cd230c8a9e
Kshitiz Sharma <skshitiz@vmware.com>
2022-07-31 12:23:17 +0530

    loading data from API | Changing chart type to horizontalBar

--- site.js
+++ site.js
@@ -1,26 +1,43 @@
 // Self executing function to avoid pollution of JavaScript's global namespace
 (
-    () => {
+    async () => {
+        var apiData = await getApiData();
+        const branches = apiData.map(o => o.branch); // Array of the values based on the property specified. 
+        const revenues2020 = apiData.map(o => o.revenue2020);
+        const revenues2019 = apiData.map(o => o.revenue2019);
+        const revenues2018 = apiData.map(o => o.revenue2018);
+        
         const ctx = document.getElementById('chart');
         new Chart(ctx, {
-            type: 'bar',
+            type: 'horizontalBar', // 'bar',
             data: {
-
-                labels: ["Seattle", "Portland", "Monterey", "San Francisco", "San Diego"],
+                labels: branches,
                 datasets: [
                     {
+                        backgroundColor: "green",
+                        label: "Revenues - 2018",
+                        data: revenues2018
+                    },                    {
                         backgroundColor: "red",
-                        label: "Revenues per branch",
-                        data: [47100, 67742, 50320, 67993, 50532]
+                        label: "Revenues - 2019",
+                        data: revenues2019
                     },
                     {
                         backgroundColor: "purple",
-                        label: "Revenues per branch",
-                        data: [43223, 64332, 53322, 67743, 51132]
+                        label: "Revenues - 2020",
+                        data: revenues2019
                     }
                 ]
             }
         });
+
+        // An async function enables the use of await in the function
+        async function getApiData() {
+            const apiResult = await fetch("https://chartjsapi.azurewebsites.net/branch/revenues");
+            const json = await apiResult.json();
+            return json;
+        }
+
     }
 )(); 
 
@@ -28,3 +45,6 @@
 /*
 Chart.js is an open source JavaScript library that lets us draw data visualizations on <canvas>
 */
+
+// Mini API
+// https://chartjsapi.azurewebsites.net/branch/revenues
